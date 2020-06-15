3327dc9c9bc663afb056f05a6c722cc77d043c83
n-ogawa <fy59.kou-729@live.jp>
2020-06-15 23:15:07 +0900

    slack通知のパイプラインを追加

--- notification/notification/pipelines.py
+++ notification/notification/pipelines.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import textwrap
 from os import environ
 
+import slackweb
 from scrapy.exceptions import DropItem
 
 
@@ -20,3 +22,19 @@ class SearchPipeline(object):
             if author_search_keyword in item['author']:
                 return item
         raise DropItem('Not Match Author')
+
+
+class SlackPipeline(object):
+    def process_item(self, item, spider):
+        webhook_url = environ.get('SINKAN_TUUTI_SLACK_URL')
+        if not webhook_url:
+            raise DropItem('webhook url is not defined.')
+
+        message = textwrap.dedent(f'''
+        著者：{item['author']}
+        タイトル：{item['title']}
+        出版日：{item['publishing_date']}
+        ''')
+
+        slack = slackweb.Slack(webhook_url)
+        slack.notify(text=message)
