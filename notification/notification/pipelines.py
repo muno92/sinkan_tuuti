bf278fee8ebfda167a678421127af9c4e1de0e09
n-ogawa <fy59.kou-729@live.jp>
2020-06-15 22:38:09 +0900

    著者名が一致する書誌情報のみ取得するよう調整

--- notification/notification/pipelines.py
+++ notification/notification/pipelines.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from os import environ
+
+from scrapy.exceptions import DropItem
+
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class NotificationPipeline(object):
+class SearchPipeline(object):
     def process_item(self, item, spider):
-        return item
+        author_search_keywords = environ.get("AUTHOR_SEARCH_KEYWORDS", "") \
+            .split()
+
+        for author_search_keyword in author_search_keywords:
+            if author_search_keyword in item['author']:
+                return item
+        raise DropItem('Not Match Author')
--- notification/notification/settings.py
+++ notification/notification/settings.py
@@ -63,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'notification.pipelines.NotificationPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'notification.pipelines.SearchPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
